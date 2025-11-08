27dd9db9251d16324bcedecaf27e2e32babd64a7
vbuddhacharya <vishabuddhacharya01@gmail.com>
2025-11-08 19:33:32 +0545

    feat: attach/detach tags from post

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\CommentController;
 use App\Http\Controllers\API\PostCommentController;
 use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\PostTagController;
 use App\Http\Controllers\API\TagController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
@@ -26,4 +27,5 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::apiResource('posts.comments', PostCommentController::class)->only(['index', 'store']);
     Route::apiResource('comments', CommentController::class)->only(['show', 'update', 'destroy']);
     Route::apiResource('tags', TagController::class);
+    Route::apiResource('posts.tags', PostTagController::class)->only(['index', 'store', 'destroy']);
 });
