828b2c74f1c1fd3da28ad7aec38932320409a12c
vbuddhacharya <vishabuddhacharya01@gmail.com>
2025-11-09 12:35:03 +0545

    feat: add middleware to check if user is admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
 
         //
